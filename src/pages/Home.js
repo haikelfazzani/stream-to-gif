ad6f96ca22dfd6e08a76d6be7d26cf572fe7d7be
Haikel Fazzani <haikel.fazzani@hotmail.com>
2020-03-13 17:48:03 +0100

    update app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ export default function Home () {
 
         <h4>Speed, meet simplicity and open source</h4>
         <p className="mt-0">Generate a beautiful gif images with one click.</p>
-        <Link className="btn btn-primary btn-lg w-50" to="/converter">
+        <Link className="btn btn-primary btn-lg w-50" to="/videos">
           <span className="mr-2">Get started now</span> <i className="fas fa-play"></i>
         </Link>
       </div>
@@ -27,19 +27,19 @@ export default function Home () {
     <p className="text-center">For whatever reason you need gif converter, we have got you covered!</p>
 
     <div className="row py-5 text-center">
-      <div className="col-md-4">
+      <div className="col-md-4 border">
         <h2><i className="fas fa-expand"></i></h2>
         <h3>Resize</h3>
         <p className="m-0">You can specify the width and the height of the gif image output.</p>
       </div>
 
-      <div className="col-md-4">
+      <div className="col-md-4 border">
         <h2><i className="fas fa-images"></i></h2>
         <h3>Choose quality</h3>
         <p className="m-0">You can choose the quality of the gif image by changing the frame duration, sample interval and interval.</p>
       </div>
 
-      <div className="col-md-4">
+      <div className="col-md-4 border">
         <h2><i className="fas fa-window-restore"></i></h2>
         <h3>Choose format</h3>
         <p className="m-0">Convert images or video to gif image.</p>
